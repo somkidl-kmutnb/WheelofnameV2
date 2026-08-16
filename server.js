1ec10075bb4af3fc51c0a425f67f18fd718e6390
somkidl-kmutnb <somkidl@kmutnb.ac.th>
2026-08-16 14:06:19 +0700

    feat: add department logo and KMUTNB footer credit

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const MIME_TYPES = {
   '.json': 'application/json; charset=UTF-8',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.jfif': 'image/jpeg',
   '.gif': 'image/gif',
   '.svg': 'image/svg+xml',
   '.ico': 'image/x-icon'
